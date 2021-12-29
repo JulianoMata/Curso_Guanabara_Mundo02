aa82ddb6256d879383abf5d6f3e120367417160f
julianomata <jfmatta@gmail.com>
2021-12-29 17:34:44 -0300

    exercicio "038" e "039" resolvidos

--- Python3-Mundo02/Condicoes_Aninhadas/exercicio_038.py
+++ Python3-Mundo02/Condicoes_Aninhadas/exercicio_038.py
@@ -2,3 +2,12 @@
  - O PRIMEIRO VALOR É MAIOR
  - O SEGUNDO VALOR É MAIOR
  - O NÃO EXISTE VALOR MAIOR, OS DOIS SÃO IGUAIS """
+
+num01 = int(input("Digite o primeiro número: "))
+num02 = int(input("Digite o segundo número: "))
+if num01 > num02:
+    print(f"Primeiro número {num01} é MAIOR que o segundo número {num02}!!!")
+elif num02 > num01:
+    print(f"O segundo número {num02} é MAIOR que o primeiro número {num01}!!!")
+else:
+    print(f"O primeiro número {num01} e o segundo numero {num02} são iguais!!! ")
--- Python3-Mundo02/Condicoes_Aninhadas/exercicio_039.py
+++ Python3-Mundo02/Condicoes_Aninhadas/exercicio_039.py
@@ -3,3 +3,23 @@
 - SE É A HORA DE ALISTAR.
 - SE JÁ PASSOU DO TEMPO DO ALISTAMENTO.
 SEU PROGRAMA TAMBÉM DEVERÁ MOSTRAR O TEMPO QUE FALTA OU QUE PASSOU DO PRAZO."""
+
+from datetime import date
+anoAtual = date.today().year
+# BUSCA A DATA ATUAL NO SISTEMA
+ano_nasc = int(input("Digite seu ano de nascimento: "))
+idade = anoAtual - ano_nasc
+print(f"Quem nasceu em {ano_nasc} tem {idade} em {anoAtual}")
+
+if idade < 18:
+    prazo = 18 - idade
+    print(f"Ainda falta(m) {prazo}ano(s) para o serviço militar")
+    anoAlistamento = anoAtual + prazo
+    print(f"Seu alistamento será em {anoAlistamento}")
+elif idade == 18:
+    print("Hora de se alistar")
+else:
+    prazo = idade - 18
+    print(f"Já serviu ou passou do prazo em {prazo}ano(s) para alistamento militar")
+    anoAlistamento = anoAtual - prazo
+    print(f"O ano de seu alistamento foi {anoAlistamento}")
