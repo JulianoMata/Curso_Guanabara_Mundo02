0b1ce733b6245aa8546c3dc1ea916b12bc28a02f
julianomata <jfmatta@gmail.com>
2021-12-29 16:36:09 -0300

    exercicio "037" resolvido

--- Python3-Mundo02/Condicoes_Aninhadas/exercicio_037.py
+++ Python3-Mundo02/Condicoes_Aninhadas/exercicio_037.py
@@ -1,6 +1,23 @@
 """ ESCREVA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E PEÇA PARA O USUÁRIO ESCOLHER QUAL SERÁ A BASE
  DE CONVERSÃO:
- -1 PAR BINÁRIO
+ -1 PARA BINÁRIO
  -2 PARA OCTAL
  -3 PARA HEXADECIMAL"""
 
+numero = int(input("Digite um número: "))
+print(""" Escolha uma das bases para conversão:
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL""")
+conversor = int(input("Digite sua opção: "))
+if conversor == 1:
+    print(f"{numero} convertido em BINÁRIO é igual a {bin(numero)[2:]}")
+    # fatiamento de 'strings' posição 2 ate o final, pois existem 2 caracteres iniciais desnecessários
+elif conversor == 2:
+    print(f"{numero} convertido em OCTAL é igual a {oct(numero)[2:]:}")
+    # fatiamento de 'strings' posição 2 ate o final, pois existem 2 caracteres iniciais desnecessários
+elif conversor == 3:
+    print(f"{numero} convertido em HEXADECIMAL é igual a {hex(numero)[2:]}")
+    # fatiamento de 'strings' posição 2 ate o final, pois existem 2 caracteres iniciais desnecessários
+else:
+    print("Número inválido!!!")
