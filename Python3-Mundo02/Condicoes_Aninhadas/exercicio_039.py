92dcf9bb15a580cec214427e5e9322855a208f05
julianomata <jfmatta@gmail.com>
2021-12-29 17:56:56 -0300

    exercicio "040" resolvido

--- Python3-Mundo02/Condicoes_Aninhadas/exercicio_039.py
+++ Python3-Mundo02/Condicoes_Aninhadas/exercicio_039.py
@@ -7,19 +7,26 @@ SEU PROGRAMA TAMBÉM DEVERÁ MOSTRAR O TEMPO QUE FALTA OU QUE PASSOU DO PRAZO.""
 from datetime import date
 anoAtual = date.today().year
 # BUSCA A DATA ATUAL NO SISTEMA
-ano_nasc = int(input("Digite seu ano de nascimento: "))
-idade = anoAtual - ano_nasc
-print(f"Quem nasceu em {ano_nasc} tem {idade} em {anoAtual}")
+sexo = int(input("Digite seu sexo\n[ 1 ] Feminino\n[ 2 ] Masculino\n"))
+if sexo == 1:
+    print("Você não precisa se alistar.")
 
-if idade < 18:
-    prazo = 18 - idade
-    print(f"Ainda falta(m) {prazo}ano(s) para o serviço militar")
-    anoAlistamento = anoAtual + prazo
-    print(f"Seu alistamento será em {anoAlistamento}")
-elif idade == 18:
-    print("Hora de se alistar")
+elif sexo == 2:
+    ano_nasc = int(input("Digite seu ano de nascimento: "))
+    idade = anoAtual - ano_nasc
+    print(f"Quem nasceu em {ano_nasc} tem {idade} em {anoAtual}")
+
+    if idade < 18:
+        prazo = 18 - idade
+        print(f"Ainda falta(m) {prazo}ano(s) para o serviço militar")
+        anoAlistamento = anoAtual + prazo
+        print(f"Seu alistamento será em {anoAlistamento}")
+    elif idade == 18:
+        print("Hora de se alistar")
+    else:
+        prazo = idade - 18
+        print(f"Já serviu ou passou do prazo em {prazo}ano(s) para alistamento militar")
+        anoAlistamento = anoAtual - prazo
+        print(f"O ano de seu alistamento foi {anoAlistamento}")
 else:
-    prazo = idade - 18
-    print(f"Já serviu ou passou do prazo em {prazo}ano(s) para alistamento militar")
-    anoAlistamento = anoAtual - prazo
-    print(f"O ano de seu alistamento foi {anoAlistamento}")
+    print("INVÁLIDO!!!")
--- Python3-Mundo02/Condicoes_Aninhadas/exercicio_040.py
+++ Python3-Mundo02/Condicoes_Aninhadas/exercicio_040.py
@@ -6,3 +6,16 @@ REPROVADO
 RECUPERAÇÃO
 - MÉDIA 7.0 OU SUPERIOR:
 APROVADO"""
+
+nota01 = float(input("Digite a primeira nota: "))
+nota02 = float(input("Digite a segunda nota: "))
+media = (nota01 + nota02)/2
+if media >= 7:
+    print("APROVADO")
+elif 5 < media < 7:
+    print("RECUPERAÇÃO")
+else:
+    print("REPROVADO")
+
+
+
