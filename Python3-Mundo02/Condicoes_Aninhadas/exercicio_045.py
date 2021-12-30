8d11ef2d99039576f69873eae2d93a062ee8b139
julianomata <jfmatta@gmail.com>
2021-12-29 21:10:19 -0300

    exercicio "045" resolvido.

--- Python3-Mundo02/Condicoes_Aninhadas/exercicio_045.py
+++ Python3-Mundo02/Condicoes_Aninhadas/exercicio_045.py
@@ -1,1 +1,54 @@
 """ CRIE UM PROGRAMA QUE FAÇA O COMPUTADOR JOGAR OKENPÔ COM VOCÊ. 'PEDRA/PAPEL/TESOURA'"""
+
+from random import randint
+from time import sleep
+itens = ("pedra", "papel", "tesoura")
+computador = randint(0, 2)
+#
+print("""Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA""")
+jogador = int(input("Qual sua jogada? "))
+print("JO")
+sleep(1)
+print("KEN...")
+sleep(1)
+print("POOO...")
+sleep(1)
+print("-=" * 20)
+print(f"O computador escolheu {itens[computador]}")
+print(f"Jogador escolheu {itens[jogador]}")
+print("-=" * 20)
+
+if computador == 0:   # jogou pedra
+    if jogador == 0:
+        print("EMPATE")
+    elif jogador == 1:
+        print("JOGADOR VENCE")
+    elif jogador == 2:
+        print("COMPUTADOR VENCE")
+    else:
+        print("Jogada inválida")
+
+
+elif computador == 1:  # jogou papel
+    if jogador == 0:
+        print("COMPUTADOR VENCE")
+    elif jogador == 1:
+        print("EMPATE")
+    elif jogador == 2:
+        print("JOGADOR VENCE")
+    else:
+        print("Jogada inválida")
+
+elif computador == 2:  # jogou tesoura
+    if jogador == 0:
+        print("JOGADOR VENCE")
+    elif jogador == 1:
+        print("COMPUTADOR VENCE")
+    elif jogador == 2:
+        print("EMPATE")
+    else:
+        print("Jogada inválida")
+
